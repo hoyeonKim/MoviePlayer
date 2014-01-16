f6822ec6deffac80dc7a8dc1967b47e73be15b5d
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-16 15:20:21 +0900

    MoviePlayer
    complete

--- media/ViewController.m
+++ media/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
 
-@interface ViewController ()
+#define MOVIE_URL @"http://movies.apple.com/media/kr/iphone/2011/tours/apple-iphone4s-feature_keynote-kr-20111110_r848-9cie.mov"
+
+@interface ViewController (){
+    MPMoviePlayerController *player;
+}
 
 @end
 
@@ -18,8 +23,32 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(handleStateChanged:) name:@"MPMoviePlayerPlaybackStateDidChangeNotification" object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(handleMediaTypeNoti:) name:@"MPMovieSourceTypeAvailableNotification" object:nil];
+    
+    //NSURL *url = [[NSBundle mainBundle]URLForResource:@"movie" withExtension:@"mp4"];
+    NSURL *url =[NSURL URLWithString:MOVIE_URL];
+    NSAssert((nil!=url),@"url is nil");
+    
+    player =[[MPMoviePlayerController alloc]initWithContentURL:url];
+    [player prepareToPlay];
+    [player.view setFrame:self.view.bounds];
+    [self.view addSubview:player.view];
 }
 
+-(void)handleMediaTypeNoti:(NSNotification *)noti{
+    MPMoviePlayerController *p = noti.object;
+    NSLog(@"Media Source Type: %d",p.movieSourceType);
+}
+
+-(void)handleStateChanged:(NSNotification *)noti{
+    MPMoviePlayerController *p = noti.object;
+    NSLog(@"Playback state : %d",p.playbackState);
+}
+-(void)viewDidUnload{
+    [super viewDidUnload];
+    [[NSNotificationCenter defaultCenter]removeObserver:self];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
